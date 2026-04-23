216987281b27653e8163d4f4eeef238055d06590
Caden Rogers <crogers3@my.waketech.edu>
2026-04-23 14:08:37 -0400

    ahhhh, i got no time, i got no time to live

--- big_code.js
+++ big_code.js
@@ -5,6 +5,59 @@ const x_start = document.getElementById("x_start");
 const y_start = document.getElementById("y_start");
 const x_end = document.getElementById("x_end");
 const y_end = document.getElementById("y_end");
+const grid_row = document.getElementsByTagName("tr");
+const grid_column = document.getElementsByTagName("td");
+x_length.value = 5;
+y_length.value = 5;
+x_start.value = 1;
+y_start.value = 1;
+x_end.value = x_length.value;
+y_end.value = y_length.value;
+// x_length.addEventListener("change", () => {
+//     });
+// y_length.addEventListener("change", () => {
+//     });
+// implement this after algorythm is working.
+x_start.addEventListener("change", () => {
+    if (x_start.value > x_length.value){ 
+        x_start.value = x_length.value;
+    }
+    if (x_start.value < 1){
+        x_start.value = 1;
+    }
+    testmaze.demolish_grid();
+    testmaze.build_grid();
+    });
+y_start.addEventListener("change", () => {
+    if (y_start.value > y_length.value){ 
+        y_start.value = y_length.value;
+    }
+    if (y_start.value < 1){
+        y_start.value = 1;
+    }
+    testmaze.demolish_grid();
+    testmaze.build_grid();
+});
+x_end.addEventListener("change", () => {
+    if (x_end.value > x_length.value){ 
+        x_end.value = x_length.value;
+    }
+    if (x_end.value < 1){
+        x_end.value = 1;
+    }
+    testmaze.demolish_grid();
+    testmaze.build_grid();
+    });
+y_end.addEventListener("change", () => {
+    if (y_end.value > y_length.value){ 
+        y_end.value = y_length.value;
+    }
+    if (y_end.value < 1){
+        y_end.value = 1;
+    }
+    testmaze.demolish_grid();
+    testmaze.build_grid();
+});
 
 class Maze{
     constructor(){
@@ -13,16 +66,14 @@ class Maze{
     build_grid(){
         this.create_table = document.createElement("table");
         this.create_table.id = "table";
-        this.y_length = 6; //remove this after i make the table dynamic
-        this.x_length = 5; //remove this after i make the table dynamic
         grid.appendChild(this.create_table);
-        for(let y=0; y<=this.y_length; y++){
+        for(let y=0; y<=y_length.value; y++){
             this.table_row = document.createElement("tr");
             this.table_row.id = y;
             this.create_table.appendChild(this.table_row);
-            for(let x=0; x<=this.x_length; x++){
+            for(let x=0; x<=x_length.value; x++){
                 this.table_column = document.createElement("td");
-                this.table_column.id = x;
+                this.table_column.id = x + "," + y;
                 this.table_row.appendChild(this.table_column);
                 this.table_column.innerHTML = x + "," + y;
             }
@@ -45,16 +96,3 @@ class tiles extends Maze{
 testmaze = new Maze() 
 
 testmaze.build_grid()
-
-y_length.addEventListener("change", () => {
-    });
-x_length.addEventListener("change", () => {
-    });
-x_start.addEventListener("change", () => {
-    });
-y_start.addEventListener("change", () => {
-    });
-x_end.addEventListener("change", () => {
-    });
-y_end.addEventListener("change", () => {
-    });
